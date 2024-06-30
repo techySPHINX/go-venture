8d112c7e1f63a79c7db77fef7b75095386af456b
techySPHINX <shimon123universe@gmail.com>
2024-06-30 19:44:20 +0530

    Modified Syntax

--- go-web-server/main.go
+++ go-web-server/main.go
@@ -1,24 +1,30 @@
 package main
 
-import{
-	"fmt","log","net/http"
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
 
-}
-
-func formHandler(w http.ResponseWriter, r "http.Request"){
-	if err := r.parseForm(); err != nil{
-		fmt.printf(w, "parseForm() err: %v", err)
+func formHandler(w http.ResponseWriter, r *http.Request) {
+	if err := r.ParseForm(); err != nil {
+		fmt.Fprintf(w, "parseForm() err: %v", err)
 		return
 	}
+	fmt.Fprintf(w, "POST request successful")
+	name := r.FormValue("name")
+	address := r.FormValue("address")
+	fmt.Fprintf(w, "Name = %s\n", name)
+	fmt.Fprintf(w, "Address = %s\n", address)
 }
 
-func helloHandler(w http.ResponseWriter, r "http.Request"){
-	if r.URL.Path  != '/hello'{
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/hello" {
 		http.Error(w, "404 not found", http.StatusNotFound)
 		return
 	}
 
-	if r.method != "GET"{
+	if r.Method != "GET" {
 		http.Error(w, "method is not supported", http.StatusNotFound)
 		return
 	}
@@ -26,14 +32,14 @@ func helloHandler(w http.ResponseWriter, r "http.Request"){
 	fmt.Fprintf(w, "hello!")
 }
 
-func main(){
+func main() {
 	fileServer := http.FileServer(http.Dir("./static"))
 	http.Handle("/", fileServer)
 	http.HandleFunc("/form", formHandler)
 	http.HandleFunc("/hello", helloHandler)
 
-	fmt.printf("Starting server at port 8080\n")
-	if err := http.ListenAndServe(":8080", nil){
+	fmt.Printf("Starting server at port 8080\n")
+	if err := http.ListenAndServe(":8080", nil); err != nil {
 		log.Fatal(err)
 	}
 }
